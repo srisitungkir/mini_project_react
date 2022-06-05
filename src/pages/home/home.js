8cc132f97025226b61a80d2d4d11f05f572d5e7e
srisitungkir <sri.situngkir39@gmail.com>
2022-06-05 22:16:53 +0700

    install and use react-loading

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,11 +3,15 @@ import { getAllProducts } from "../../services/api";
 import CardComponent from "../../components/card/card_component";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import ReactLoading from 'react-loading';
+
 
 const Home = () => {
   const [dataProducts, setDataProducts] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false)
 
   const fetchGetAllProduct = async () => {
+    await setIsLoading(true)
     await getAllProducts()
       .then((response) => {
         setDataProducts(response.data);
@@ -15,6 +19,7 @@ const Home = () => {
       .catch((error) => {
         console.log(error);
       });
+    await setIsLoading(false)
   };
 
   useEffect(() => {
@@ -23,7 +28,8 @@ const Home = () => {
 
   return (
     <div>
-      <Container className="mt-4">
+      {IsLoading ? ( <ReactLoading type="spinningBubbles" color="#0D6EFD" className="m-auto mt-5" />) : 
+      ( <Container className="mt-4">
         <div className="d-flex flex-wrap">
           {dataProducts.map((product) => (
             <Link to="/detail_product" className="text-decoration-none">
@@ -36,6 +42,7 @@ const Home = () => {
           ))}
         </div>
       </Container>
+      )}
     </div>
   );
 };
